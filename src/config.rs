57254faa4af4631b710bf484d493377fd98da858
Kevin Liu <kevin@kliu.io>
2021-05-27 18:50:32 -0400

    Split config functions into config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,55 @@
+use anyhow::Error;
+use anyhow::Result;
+use futures::StreamExt;
+use inotify::{Inotify, WatchMask};
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+use std::{fs, io::ErrorKind};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     pub run: Vec<String>,
+}
+
+pub fn get_config_file() -> Result<String> {
+    let xdg_dirs = xdg::BaseDirectories::new()?;
+    let config_path = xdg_dirs.get_config_home().join("wumpusd.yml");
+    Ok(String::from(config_path.to_string_lossy()))
+}
+
+pub fn read_config_file() -> Result<Config> {
+    let config_path = get_config_file()?;
+    println!("Loading config from {}", &config_path);
+
+    let mut config_contents = fs::read_to_string(&config_path);
+    match config_contents {
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            let mut file = File::create(&config_path)?;
+            file.write_all(include_bytes!("config.yml"))?;
+            config_contents = fs::read_to_string(&config_path);
+        }
+        Err(e) => return Err(Error::new(e)),
+        Ok(ref _contents) => {}
+    }
+    let config = serde_yaml::from_str(&config_contents?)?;
+
+    Ok(config)
+}
+
+pub async fn wait_for_config_change() -> Result<()> {
+    let mut inotify = Inotify::init().expect("Error while initializing inotify instance");
+
+    // Watch for modify and close events.
+    inotify
+        .add_watch(get_config_file()?, WatchMask::MODIFY | WatchMask::CLOSE)
+        .expect("Failed to add file watch");
+
+    // Read events that were added with `add_watch` above.
+    let buffer = [0; 1024];
+    let mut events = inotify.event_stream(buffer)?;
+
+    events.next().await;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,13 @@
 mod config;
+mod service_manager;
 
-use anyhow::{Error, Result};
+use anyhow::Result;
 use futures::future;
 use futures::future::FusedFuture;
 use futures::select;
 use futures::FutureExt;
-use futures::StreamExt;
-use inotify::{Inotify, WatchMask};
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::*;
-use std::{fs, io::ErrorKind};
 use tokio_util::sync::CancellationToken;
-
-mod service_manager;
-
-fn get_config_file() -> Result<String> {
-    let xdg_dirs = xdg::BaseDirectories::new()?;
-    let config_path = xdg_dirs.get_config_home().join("wumpusd.yml");
-    Ok(String::from(config_path.to_string_lossy()))
-}
-
-fn read_config_file() -> Result<config::Config> {
-    let config_path = get_config_file()?;
-    println!("Loading config from {}", &config_path);
-
-    let mut config_contents = fs::read_to_string(&config_path);
-    match config_contents {
-        Err(e) if e.kind() == ErrorKind::NotFound => {
-            let mut file = File::create(&config_path)?;
-            file.write_all(include_bytes!("config.yml"))?;
-            config_contents = fs::read_to_string(&config_path);
-        }
-        Err(e) => return Err(Error::new(e)),
-        Ok(ref _contents) => {}
-    }
-    let config: config::Config = serde_yaml::from_str(&config_contents?)?;
-
-    Ok(config)
-}
-
-async fn wait_for_config_change() -> Result<()> {
-    let mut inotify = Inotify::init().expect("Error while initializing inotify instance");
-
-    // Watch for modify and close events.
-    inotify
-        .add_watch(get_config_file()?, WatchMask::MODIFY | WatchMask::CLOSE)
-        .expect("Failed to add file watch");
-
-    // Read events that were added with `add_watch` above.
-    let buffer = [0; 1024];
-    let mut events = inotify.event_stream(buffer)?;
-
-    events.next().await;
-
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -64,7 +16,7 @@ async fn main() -> Result<()> {
     let mut services = HashMap::new();
 
     loop {
-        let config = read_config_file()?;
+        let config = config::read_config_file()?;
 
         // Add new services
         for command in &config.run {
@@ -82,7 +34,7 @@ async fn main() -> Result<()> {
             }
         }
 
-        // Cancel services that are no longer wanted
+        // Cancel services that are no longger wanted
         for (cmd, (_, cancellation_token)) in &mut services {
             if !config.run.contains(cmd) {
                 cancellation_token.cancel();
@@ -97,7 +49,7 @@ async fn main() -> Result<()> {
                 println!("All services have stopped / no services listed; exiting");
                 return Ok(());
             }
-            _ = wait_for_config_change().fuse() => {
+            _ = config::wait_for_config_change().fuse() => {
                 println!("Config file changed; reloading");
             }
         }
